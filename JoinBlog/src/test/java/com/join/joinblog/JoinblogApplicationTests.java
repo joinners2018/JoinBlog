655b959e6657c1727febe29ffc5d979294eddcda
VigoWeiGou <136993561@qq.com>
2019-09-01 16:12:31 +0800

    重新上传 src 等

---
+++ JoinBlog/src/test/java/com/join/joinblog/JoinblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.join.joinblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
